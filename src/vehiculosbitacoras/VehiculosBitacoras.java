9121b4a7dca906811d69af11ba8291c6cdfa30a8
Maria Paula <Maria Paula@Maria>
2020-03-31 16:30:54 -0600

    //programar

--- src/vehiculosbitacoras/VehiculosBitacoras.java
+++ src/vehiculosbitacoras/VehiculosBitacoras.java
@@ -17,6 +17,7 @@ public class VehiculosBitacoras {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
+        System.out.println("Hola mundo wiiiiiiiiiiiiiiiiii");
     }
     
 }
