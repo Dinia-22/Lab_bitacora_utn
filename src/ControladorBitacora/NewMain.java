ba25f1764a0606f3452d589786a629533dbc7fa4
Maria Paula <Maria Paula@Maria>
2020-04-01 14:46:42 -0600

    //programar

---
+++ src/ControladorBitacora/NewMain.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ControladorBitacora;
+
+import BitacoraModelo.Bitacora;
+
+/**
+ *
+ * @author Maria Paula
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Bitacora h = new Bitacora();
+        Controlador_Bitacora prueba = new Controlador_Bitacora();
+        prueba.conectar();
+        prueba.Agregar(h);
+        //prueba.buscar(" ABCD ");
+        
+        
+        
+        
+    }
+    
+}
