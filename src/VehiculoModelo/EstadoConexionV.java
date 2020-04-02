a704949dd2b6f6b4f295fc75ea8b82a39db14fd6
Adriel Chavarría B <Adriel Chavarra B@DESKTOP-GM47FTQ>
2020-04-02 16:34:09 -0600

    Mi primer comentario

---
+++ src/VehiculoModelo/EstadoConexionV.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VehiculoModelo;
+
+import VehiculoControlador.controladorVehiculo;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Adriel Chavarría B
+ */
+public class EstadoConexionV {
+
+    static Statement sentencia;
+    static ResultSet resultado;
+    controladorVehiculo consulta = new controladorVehiculo();
+
+    public void consultaEstadoV() {
+        consulta.conectar();
+
+    }
+
+    public static Vehículo filtarlista(int id, String placa, String descripcion) {
+        Vehículo r = null;
+        String q = "SELECT * FROM Datos" + " WHERE Nombre='" + descripcion + "'";
+        try {
+            resultado = sentencia.executeQuery(q);
+            System.out.println("Correcto");
+        } catch (Exception e) {
+            System.out.println(" No Correcto");
+        }
+        r = asignar();
+        return r;
+
+    }
+
+    public static Vehículo asignar() {
+        Vehículo r = null;
+        String placa, descripcion;
+        int id;
+        try {
+            if (resultado.last()) {
+                id = resultado.getInt("id");
+                placa = resultado.getString("placa");
+                descripcion = resultado.getString("descripcion");
+                r = new Vehículo(id, placa, descripcion);
+            }
+        } catch (Exception e) {
+        }
+
+        return r;
+    }
+}
